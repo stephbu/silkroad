b90857eb06e044f607fc58afa478da75b76654c9
stephbu <stephbu@gmail.com>
2015-06-24 22:35:05 -0700

    Module classes

--- Assets/Ship.cs
+++ Assets/Ship.cs
@@ -2,21 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Ship : MonoBehaviour {
+public class Ship {
 
 	public float FuelRemaining;
 	public float FuelCapacity;
 	public float ConsumptionPerParsec;
 
 	public List<Pallet> Pallets = new List<Pallet>();
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}	
+	public List<Module> Modules = new List<Module>();
 }

55d0fc5c82048c17a598b8b021b50d58cce721a3
stephbu <stephbu@gmail.com>
2015-06-24 21:56:40 -0700

    Unity Assets

---
+++ Assets/Ship.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Ship : MonoBehaviour {
+
+	public float FuelRemaining;
+	public float FuelCapacity;
+	public float ConsumptionPerParsec;
+
+	public List<Pallet> Pallets = new List<Pallet>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}	
+}
---
+++ Assets/State.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+public class State : MonoBehaviour {
+
+	public string GameSeed;
+
+	// Use this for initialization
+	void Start () {
+		// load
+
+		if (this.GameSeed == null) {
+			this.GameSeed = string.Concat(RandomHexChar().Take(10));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private string hexChars = "0123456789ABCDEF";
+	private System.Random rand = new System.Random((int) (DateTime.Now.Ticks & Int32.MaxValue));
+
+	IEnumerable<char> RandomHexChar()
+	{
+		while (true) {
+			yield return hexChars[rand.Next(15)];
+		}
+	}
+}
